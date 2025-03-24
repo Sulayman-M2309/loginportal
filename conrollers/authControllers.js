eb324909b90b9272ac338740bcfad44f2661432c
Sulayman-M2309 <abirstu9@gmail.com>
2025-03-24 11:43:52 +0600

    login part

--- conrollers/authControllers.js
+++ conrollers/authControllers.js
@@ -1,39 +1,39 @@
 const { sendEmail } = require("../helpers/sendEmail");
 const userModels = require("../models/userModels");
-const otpGenerator = require('otp-generator')
-const bcrypt = require('bcrypt');
-async function signUpController(req, res) {
-  const { phone, password, email, name, role } = req.body;
-  const otp = otpGenerator.generate(6, { upperCaseAlphabets: false, specialChars: false });;
-  // send Database
+var jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const aleaRNGFactory = require("number-generator/lib/aleaRNGFactory");
+async function signupController(req, res) {
+  const { name, email, password, phone, role } = req.body;
+  const otp = aleaRNGFactory(Date.now()).uInt32().toString().substring(0, 6);
   try {
     const olduseremail = await userModels.findOne({ email });
-    // check email
-    // res.send(olduseremail)
     if (!olduseremail) {
-        
-      const user = new userModels({
-        name,
-        email,
-        password,
-        phone,
-        role,
-      });
-      await user.save();
+      bcrypt.hash(password, 10, async function (err, hash) {
+        // Store hash in your password DB.
+        const user = new userModels({
+          name,
+          email,
+          password: hash,
+          phone,
+          role,
+        });
+        await user.save();
         sendEmail(email, otp);
         user.otp = otp;
         await user.save();
-        //   otp time validatio
+
+        // otp time validatio
         //   setTimeout( async()=>{
         // user.otp=null;
         // await user.save()
         //   },120000)
-      res
-        .status(201)
-        .json({ msg: "signup crating Success", success: true, data: user });
-    }
-    else{
         res
+          .status(201)
+          .json({ msg: "signup crating Success", success: true, data: user });
+      });
+    } else {
+      res
         .status(500)
         .json({ msg: "Alrady You have an a Account", success: false });
     }
@@ -43,7 +43,56 @@ async function signUpController(req, res) {
       .json({ error: error.message ? error.message : message, success: false });
   }
 }
+// Login part 
 async function loginController(req, res) {
-  res.send("Login");
+  const { email, password } = req.body;
+try {
+  const existinguser = await userModels.findOne({ email });
+  if (!existinguser) {
+    return res.status(404).json({ success: false, msg: "email not found" });
+  }else{
+    bcrypt.compare(password,existinguser.password,async function(err, result) {
+      const user = await userModels.findOne({ email }).select("-password");
+      // res.send(result)
+      if(result){
+        return res
+        .status(200)
+        .json({ success: true, msg: "Login Successfully",data:user });
+
+      }
+      else{
+        return res
+        .status(404)
+        .json({ msg: "Invalid Password", success: false });
+      }
+  });
+  }
+} catch (error) {
+  res
+  .status(500)
+  .json({ error: error.message ? error.message : message, success: false });
 }
-module.exports = { signUpController, loginController };
+}
+// otp verify system
+async function verifyotpController(req, res) {
+  const { email, otp } = req.body;
+  try {
+    const existinguser = await userModels.findOne({ email });
+    if (existinguser) {
+      if (existinguser.otp == otp) {
+        (existinguser.isverify = true), (existinguser.otp = null);
+        await existinguser.save();
+        res.status(200).json({ success: true, msg: "otp successful" });
+      } else {
+        res.status(404).json({ error: "otp invalid", success: false });
+      }
+    } else {
+      res.status(404).json({ error: "user not found", success: false });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: error.message ? error.message : message, success: false });
+  }
+}
+module.exports = { signupController, loginController,verifyotpController };
--- route/auth/index.js
+++ route/auth/index.js
@@ -1,10 +1,12 @@
 const express=require('express')
-const { signUpController,loginController } = require('../../conrollers/authControllers')
+const { signupController,loginController, verifyotpController } = require('../../conrollers/authControllers')
 const router=express.Router()
 
 // http://localhost:5000/auth/signup
-router.post("/signup",signUpController)
+router.post("/signup",signupController)
 
 // http://localhost:5000/auth/login
 router.post("/login",loginController)
+// http://localhost:5000/auth/verifyotp
+router.post("/verifyotp",verifyotpController)
 module.exports=router
